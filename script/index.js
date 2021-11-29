81a0685bffac469321e13ab4fc76b7bc24ab1fc5
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-11-29 15:17:50 +0300

    Programmaticaly added transform styles for list items

--- script/index.js
+++ script/index.js
@@ -1,12 +1,32 @@
 import getContactsList from './getContactsList.js';
 import renderContactsList from './renderContactsList.js';
 
+
+/*
+Programmaticaly add transform styles for list items
+*/
+const addContactsListStyles = (contactList) => {
+  let style = '';
+  for (let i = 11; i <= contactList.length; i++ ) {
+    const offset = (i - 1) * 70;
+    style += `
+#container .list-view ul li:nth-child(${i}) {
+  -webkit-transform: translate3d(0, ${offset}px, 0);
+          transform: translate3d(0, ${offset}px, 0);
+}`;
+  }
+  const styleEl = document.createElement('style');
+  styleEl.innerHTML = style;
+  document.body.appendChild(styleEl);
+};
+
 (async () => {
   try {
-    const peopleList = await getContactsList();
+    const contactsList = await getContactsList();
+    addContactsListStyles(contactsList);
     const container = document.getElementById('container');
-    renderContactsList(container, peopleList);
+    renderContactsList(container, contactsList);
   } catch {
-    console.log('Failed to load people list');
+    console.log('Failed to load contacts list');
   }
 })();
