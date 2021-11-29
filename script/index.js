0176f00fe6bb1337a5380d7560f1487e8863d06c
Alexey Desyatnik <alexey.desyatnik@gmail.com>
2021-11-29 11:05:55 +0300

    Implemented loading data.json via fetch (requires dev server, doesn't work with local files)

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
-console.log('Hi there');
+const peopleListUrl = '../data.json';
+
+const loadPeople = async () => {
+  const res = await fetch(peopleListUrl);
+  const peopleList = await res.json();
+  return peopleList;
+};
+
+(async () => {
+  try {
+    const peopleList = await loadPeople();
+    console.log(peopleList);
+  } catch {
+    console.log('Failed to load people list');
+  }
+})();
